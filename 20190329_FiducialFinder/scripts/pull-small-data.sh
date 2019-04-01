1e74cda6cc9cc1242a7e5148cfa177662974d35e
abpwrs <alexander-powers@uiowa.edu>
2019-04-01 13:49:47 -0500

    using ssh

--- 20190329_FiducialFinder/scripts/pull-small-data.sh
+++ 20190329_FiducialFinder/scripts/pull-small-data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 temp_dir=/tmp/temporary_data_`date +%Y%m%d`
-git clone https://github.uiowa.edu/SINAPSE/UIOWA2018_LesionMapping.git $temp_dir 
+git clone git@github.uiowa.edu:SINAPSE/UIOWA2018_LesionMapping.git $temp_dir 
 cp $temp_dir/SmallData/* ./SmallData/
 rm -rf $temp_dir
 
