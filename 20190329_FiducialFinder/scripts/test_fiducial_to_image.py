73e9f4fd06006129b91bdeeb63c497361f1472d7
Chase Johnson <cjohnson30@uiowa.edu>
2019-04-23 16:08:27 -0500

    Created a python script that generates the same output of fiducial_to_image for testing fiducial_to_image.

---
+++ 20190329_FiducialFinder/scripts/test_fiducial_to_image.py
@@ -0,0 +1,78 @@
+
+# coding: utf-8
+
+# In[38]:
+
+
+import SimpleITK as sitk
+import os
+import json
+import pandas as pd
+import csv
+from fiducial_to_image import extract_fiducials
+import ntpath
+
+
+# In[76]:
+
+
+for file in os.listdir("../SmallData/"):
+    base, _ = os.path.splitext(file)
+    base, _ = os.path.splitext(base)
+
+
+# In[10]:
+
+
+# returns "_RE_LE"
+def get_fiducial_file_identifier():
+    fid_file_identifier = ""
+    fids = get_fiducial_keys()
+    for fid in fids:
+        fid_file_identifier += "_" + fid
+    return fid_file_identifier
+
+
+# In[7]:
+
+
+# Returns ['RE', 'LE']
+def get_fiducial_keys():
+    fids = []
+    with open("../fiducials.json", "rb") as f:
+        fids = json.load(f)['fiducials']
+    return fids
+
+
+# In[52]:
+
+
+
+def get_expected_fiducial_data(file_path):
+    fcsv_filename = ntpath.basename(file_path)
+    image_name_prefix, _ = os.path.splitext(fcsv_filename)
+    image_name = image_name_prefix + "_t1w.nii.gz"
+    image_path = file_path.replace(fcsv_filename, "") + image_name
+    image = sitk.ReadImage(image_path)
+    ordered_fiducials = extract_fiducials(file_path)
+    expected_fiducial_data = []
+    for fiducial_key in get_fiducial_keys():
+        expected_index_data = image.TransformPhysicalPointToIndex(ordered_fiducials[fiducial_key])
+        expected_fiducial_data.extend(expected_index_data)
+    return expected_fiducial_data
+
+
+# In[58]:
+
+
+def main():
+    file_path = "../SmallDataTest/0001_27588.fcsv"
+    print(get_expected_fiducial_data(file_path))
+
+
+# In[59]:
+
+
+if __name__ == "__main__":
+    main()
+
